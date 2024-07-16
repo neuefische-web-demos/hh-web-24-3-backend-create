7406f451d4e16740ff8a7e0545c01def6e5f9c1f
Jessica Lörs <jessica.loers@gmail.com>
2024-07-16 17:50:54 +0200

    add submit handler function

--- components/JokesForm.js
+++ components/JokesForm.js
@@ -1,6 +1,19 @@
+import useSWR from 'swr';
+
 export default function JokeForm() {
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    const formData = new FormData(event.target);
+    const jokedata = Object.fromEntries(formData);
+
+    console.log('The New Joke', jokedata);
+
+    event.target.reset();
+  }
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <label htmlFor="joke-input">Enter a new joke</label>
       <input type="text" id="joke-input" name="joke" />
       <button type="submit">Submit</button>
